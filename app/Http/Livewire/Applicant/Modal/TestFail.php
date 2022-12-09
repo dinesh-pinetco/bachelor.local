759bdc9722dc717ef56249f5c675fe33157f941b
Padhiyar Kishan <kishan@pinetco.com>
2022-12-09 15:04:32 +0530

    employee can confirm that applicant is failed

---
+++ app/Http/Livewire/Applicant/Modal/TestFail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire\Applicant\Modal;
+
+use App\Http\Livewire\Traits\HasModal;
+use App\Models\Result;
+use App\Models\User;
+use Livewire\Component;
+
+class TestFail extends Component
+{
+    use HasModal;
+
+    public User $applicant;
+
+    public function render()
+    {
+        return view('livewire.applicant.modal.test-fail');
+    }
+
+    public function toggle(User $user)
+    {
+        $this->show = ! $this->show;
+        $this->applicant = $user;
+    }
+
+    public function confirmFail()
+    {
+        $this->applicant->applicantFailedSelectionTest();
+        $this->emitUp('refresh');
+        $this->close();
+    }
+}
