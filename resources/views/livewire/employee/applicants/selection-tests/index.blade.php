3f2e24a08cc00bf21f4b409e8dd0b563011287b6
Pooja Jadav <contact2pooja36@gmail.com>
2022-12-13 12:12:20 +0530

    Selection test result view changed

--- resources/views/livewire/employee/applicants/selection-tests/index.blade.php
+++ resources/views/livewire/employee/applicants/selection-tests/index.blade.php
@@ -1,14 +1,16 @@
 <div class="max-w-screen-xl w-full mx-auto">
-    <livewire:tabs :applicant="$applicant" />
+    <livewire:tabs :applicant="$applicant"/>
     <div class="flex flex-wrap relative">
         <div class="hidden lg:block flex-shrink-0 w-20 md:w-32 lg:w-40 2xl:w-64">
             <div class="flex items-center justify-center py-8 lg:py-6 xl:py-0"></div>
         </div>
         <div class="flex-grow space-y-4">
-            <x-danger-button
-                wire:click="$emit('Applicant.Modal.TestFail.modal.toggle',{{ $applicant->id }})">
-                {{ __('Failed Confirm') }}
-            </x-danger-button>
+            @if($applicant->application_status == \App\Enums\ApplicationStatus::TEST_FAILED)
+                <x-danger-button
+                    wire:click="$emit('Applicant.Modal.TestFail.modal.toggle',{{ $applicant->id }})">
+                    {{ __('Failed Confirm') }}
+                </x-danger-button>
+            @endif
             @if ($isShow)
                 @foreach ($tests as $test)
                     <div class="p-4 shadow-md w-full rounded">
@@ -16,8 +18,8 @@
                         <div class="flex items-center justify-between flex-wrap">
                             <p class="text-sm md:text-base">{{ $test->status($applicant) }}</p>
                             <x-link-button :active="true"
-                                href="{{ route('employee.selection-tests.show', ['applicant' => $applicant, 'selection_test' => $test]) }}"
-                                class="items-center -mt-0">
+                                           href="{{ route('employee.selection-tests.show', ['applicant' => $applicant, 'selection_test' => $test]) }}"
+                                           class="items-center -mt-0">
                                 {{ __('Show result') }}
                             </x-link-button>
                         </div>
