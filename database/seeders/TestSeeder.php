42ac1907fb0b7d6a6fa27b403964ab21e03d75af
Padhiyar Kishan <kishan@pinetco.com>
2023-02-23 15:11:40 +0530

    translation

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -16,13 +16,13 @@ class TestSeeder extends Seeder
     public function run()
     {
         foreach (config('services.moodle.types') as $name => $test) {
-            $this->createMoodleTest($name, data_get($test, 'course_id'));
+            $this->createMoodleTest(__($name), data_get($test, 'course_id'));
         }
 
         Test::create([
             'name' => 'Cubia',
             'type' => Test::TYPE_CUBIA,
-            'description' => 'A final examination, annual, exam, final interview, or simply final, is a test given to students at the end of a course of study or training. Although the term can be used in the context of physical training, it most often occurs in the academic world.',
+            'description' => __('A final examination, annual, exam, final interview, or simply final, is a test given to students at the end of a course of study or training. Although the term can be used in the context of physical training, it most often occurs in the academic world.'),
             'duration' => '60.00',
             'is_required' => true,
             'is_active' => true,
@@ -32,7 +32,7 @@ class TestSeeder extends Seeder
         Test::create([
             'name' => 'Meteor16',
             'type' => Test::TYPE_METEOR,
-            'description' => 'A final examination, annual, exam, final interview, or simply final, is a test given to students at the end of a course of study or training. Although the term can be used in the context of physical training, it most often occurs in the academic world.',
+            'description' => __('A final examination, annual, exam, final interview, or simply final, is a test given to students at the end of a course of study or training. Although the term can be used in the context of physical training, it most often occurs in the academic world.'),
             'duration' => '30.00',
             'is_required' => true,
             'is_active' => true,
@@ -47,7 +47,7 @@ class TestSeeder extends Seeder
             'name' => 'Moodle: '.$name,
             'course_id' => $course_id,
             'type' => Test::TYPE_MOODLE,
-            'description' => 'Entrance Examination is the mode for getting admission into various undergraduate, post graduate and professional degree courses. Basically entrance examination is common at higher level of education which is conducted by educational institutes and colleges.',
+            'description' => __('Entrance Examination is the mode for getting admission into various undergraduate, post graduate and professional degree courses. Basically entrance examination is common at higher level of education which is conducted by educational institutes and colleges.'),
             'duration' => '60.00',
             'is_required' => true,
             'is_active' => true,
