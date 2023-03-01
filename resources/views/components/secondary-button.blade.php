3bb779a6f0edcbd7babf9f1420124ec97b3237a3
ronakpinetco <ronak@pinetco.com>
2023-03-01 17:13:42 +0530

    btn ui improvement.

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex justify-center items-center px-4 py-2.5 lg:px-5 lg:py-3 bg-white text-primary text-center text-sm border rounded-sm border-primary duration-300 hover:bg-primary hover:text-white cursor-pointer']) }} wire:loading.attr='disabled' wire:loading.class='opacity-80 cursor-wait'>
+<a {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex justify-center items-center px-4 py-2.5 lg:px-5 lg:py-3 bg-white text-primary text-center text-base border rounded-sm font-medium border-primary duration-300 hover:bg-primary hover:text-white cursor-pointer']) }} wire:loading.attr='disabled' wire:loading.class='opacity-80 cursor-wait'>
     {{ $slot }}
 </a>
