c114322452c8f6e3a11bcc6f8d283e16b1e24654
Pooja Jadav <contact2pooja36@gmail.com>
2023-03-14 14:06:52 +0530

    Show message on applicant panel if he is hired

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::middleware(['auth:sanctum', 'verified', 'activated', 'role:'.ROLE_APPLICA
         Route::get('faq', FaqController::class)->name('faq.index');
     });
 
-    Route::get('update-password',function(){
+    Route::get('update-password', function () {
         return view('livewire.application.update-password');
     })->name('update-password');
 
