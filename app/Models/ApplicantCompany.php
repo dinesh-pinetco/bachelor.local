111036a9cfeb02b74edd9435106c7d55c56777cc
Padhiyar Kishan <kishan@pinetco.com>
2023-03-20 10:24:22 +0530

    remove unnecessary import class

--- app/Models/ApplicantCompany.php
+++ app/Models/ApplicantCompany.php
@@ -2,10 +2,8 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
 
 class ApplicantCompany extends Model
 {
