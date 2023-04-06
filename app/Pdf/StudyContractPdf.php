1c486bdec6c8dd19058bf717d3b8c0ffdc3c39ca
Padhiyar Kishan <kishan@pinetco.com>
2023-04-06 11:53:09 +0530

    WIP: Study contract pdf

---
+++ app/Pdf/StudyContractPdf.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Pdf;
+
+use App\Models\Course;
+use App\Models\User;
+use Rjchauhan\LaravelFiner\Pdf\Pdf;
+
+class StudyContractPdf extends Pdf
+{
+    protected User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function data()
+    {
+        return [
+            'user' => $this->user,
+            'desiredBeginning' => $this->user->desiredBeginning->course_start_date,
+            'street_no' => $this->user->study_sheet?->street,
+            'zip' => $this->user->study_sheet?->zip,
+            'city' => $this->user->study_sheet?->place,
+            'course' => Course::where('id', $this->user->getEctsPointvalue('enroll_course'))->first()?->name,
+        ];
+    }
+}
