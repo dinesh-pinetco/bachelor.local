549232cb63119e3b1b1fd4474ca04d1354de0331
Padhiyar Kishan <kishan@pinetco.com>
2023-05-03 16:36:53 +0530

    add command for dump data

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,35 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('clean-dump:testing', function () {
+    DB::table('applicant_companies')->truncate();
+    DB::table('contracts')->truncate();
+    DB::table('desired_beginnings')->truncate();
+    DB::table('government_forms')->truncate();
+    DB::table('meteors')->truncate();
+    DB::table('moodles')->truncate();
+    DB::table('results')->truncate();
+    DB::table('study_sheets')->truncate();
+    DB::table('user_configurations')->truncate();
+    DB::table('user_hubspot_configurations')->truncate();
+
+    User::all()->each(function ($user) {
+        if($user->hasRole(ROLE_APPLICANT)){
+            DB::table('media')->where('user_id', $user->id)->truncate();
+            DB::table('audits')->where('user_id', $user->id)->truncate();
+            DB::table('field_values')->where('user_id', $user->id)->truncate();
+            DB::table('meta')->where('user_id', $user->id)->truncate();
+            DB::table('model_has_courses')->where('user_id', $user->id)->truncate();
+            DB::table('model_has_roles')->where('user_id', $user->id)->truncate();
+            DB::table('sessions')->where('user_id', $user->id)->truncate();
+        }
+    });
+
+    User::role(ROLE_APPLICANT)->forceDelete();
+    User::all()->each(function ($user) {
+        $user->update(['password' => Hash::make('nak@123#')]);
+    });
+
+    $this->comment('Success');
+});
