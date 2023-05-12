37abab44c68c561999536c1bd45f81baa1f13ea8
Dinesh Mali <87762920+dinesh-pinetco@users.noreply.github.com>
2023-05-12 16:38:07 +0530

    Add anonymous user button applicant detail page in admin side

---
+++ app/Http/Livewire/Applicant/Modal/Anonymous.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire\Applicant\Modal;
+
+use App\Http\Livewire\Traits\HasModal;
+use App\Models\User;
+use App\Services\MakeAnonymousUser;
+use Livewire\Component;
+
+class Anonymous extends Component
+{
+    use HasModal;
+    public User $applicant;
+
+    public function toggle(User $user)
+    {
+        $this->show = ! $this->show;
+        $this->applicant = $user;
+    }
+
+    public function AnonymousApplicant()
+    {
+        MakeAnonymousUser::make($this->applicant)->execute();
+        $this->emitUp('refresh');
+        $this->close();
+        $this->toastNotify(__('Applicant has been enrolled successfully.'));
+
+    }
+
+    public function render()
+    {
+        return view('livewire.applicant.modal.anonymous');
+    }
+}
