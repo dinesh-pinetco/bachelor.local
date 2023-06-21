00b94343b98791950b57f144486ea527d6b36f3f
Deekshay <124263438+deekkshay@users.noreply.github.com>
2023-06-21 11:26:44 +0530

    Company page redesign

--- resources/views/livewire/company/index.blade.php
+++ resources/views/livewire/company/index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="w-full max-w-screen-xl mx-auto lg:pl-40 2xl:pl-64">
         <div class="flex">
-            <h1 class="mb-5 md:mb-9 text-primary text-2xl md:text-3xl lg:text-5xl font-light leading-tight">
+            <h1 class="mb-5 md:mb-10 text-primary text-2xl md:text-3xl lg:text-5xl font-thin leading-tight">
                 {{ __('Partner companies') }}
             </h1>
         </div>
@@ -142,8 +142,8 @@
         @endif
         <div class="flex-grow max-w-4xl">
             @if((is_null($user->show_application_on_marketplace_at) && is_null($user->reject_marketplace_application_at)) && auth()->user()->companies()->exists())
-                <div class="inline-flex justify-center space-x-4 text-white p-4 bg-darkgreen rounded-sm mx-auto mb-5">
-                    <svg class="text-white stroke-current w-6 h-6 flex-shrink-0" width="44" height="44"
+                <div class="inline-flex justify-center space-x-2 text-darkgreen mx-auto lg:mb-10 mb-6">
+                    <svg class="text-darkgreen stroke-current w-6 h-6 flex-shrink-0" width="44" height="44"
                          viewBox="0 0 44 44"
                          fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -154,54 +154,45 @@
                         {!! trans('Congratulations! You have applied to the following companies. You can edit the selected companies or add more at any time.')!!}
                     </p>
                 </div>
-                <h6 class="text-lg lg:text-2xl font-medium text-primary mb-2">{{ __('Appllied Company') }}: </h6>
+                <h6 class="text-black text-xs font-light mb-3">{{ __('Appllied Company') }}: </h6>
 
-                <div class="flex flex-wrap gap-2">
+                <div class="flex flex-wrap gap-2 mb-10">
                     @foreach ($appliedCompanies as $appliedCompany)
                         <div class="text-xs py-2 px-4 bg-primary bg-opacity-10 rounded-sm" id="company">
                             {{ $appliedCompany->company->name }}
                         </div>
                     @endforeach
                 </div>
-                <div
-                    class="inline-flex justify-center space-x-4 text-primary p-4 bg-secondary  rounded-sm mx-auto mt-5">
-                    <svg class="text-primary fill-current w-5 h-5" width="24" height="24" viewBox="0 0 24 24"
-                         fill="currentColor"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M12 2C10.0222 2 8.08879 2.58649 6.4443 3.6853C4.79981 4.78412 3.51809 6.3459 2.76121 8.17317C2.00433 10.0004 1.8063 12.0111 2.19215 13.9509C2.578 15.8907 3.53041 17.6725 4.92894 19.0711C6.32746 20.4696 8.10929 21.422 10.0491 21.8079C11.9889 22.1937 13.9996 21.9957 15.8268 21.2388C17.6541 20.4819 19.2159 19.2002 20.3147 17.5557C21.4135 15.9112 22 13.9778 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7363 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2ZM12 20C10.4178 20 8.87104 19.5308 7.55544 18.6518C6.23985 17.7727 5.21447 16.5233 4.60897 15.0615C4.00347 13.5997 3.84504 11.9911 4.15372 10.4393C4.4624 8.88743 5.22433 7.46197 6.34315 6.34315C7.46197 5.22433 8.88743 4.4624 10.4393 4.15372C11.9911 3.84504 13.5997 4.00346 15.0615 4.60896C16.5233 5.21447 17.7727 6.23984 18.6518 7.55544C19.5308 8.87103 20 10.4177 20 12C20 14.1217 19.1572 16.1566 17.6569 17.6569C16.1566 19.1571 14.1217 20 12 20Z"
-                            fill="currentColor"/>
-                        <path
-                            d="M12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z"
-                            fill="currentColor"/>
-                        <path
-                            d="M12 7C11.7348 7 11.4804 7.10536 11.2929 7.29289C11.1054 7.48043 11 7.73478 11 8V13C11 13.2652 11.1054 13.5196 11.2929 13.7071C11.4804 13.8946 11.7348 14 12 14C12.2652 14 12.5196 13.8946 12.7071 13.7071C12.8946 13.5196 13 13.2652 13 13V8C13 7.73478 12.8946 7.48043 12.7071 7.29289C12.5196 7.10536 12.2652 7 12 7Z"
-                            fill="currentColor"/>
-                    </svg>
-                    <p class="text-sm">
-                        {!! trans('Would you also like to be listed on the applicant marketplace so that you become visible to all partner companies and they can even contact you directly?') !!}
-                    </p>
+                <div class="border border-[#163976] rounded-sm lg:p-8 p-5 space-y-6">
+                    <div>
+                        <h3 class="text-lg font-medium text-primary mb-2"> {{__('Would you also like to be listed on the applicant marketplace?')}} </h3>
+
+                        <p class="text-sm text-black font-light">
+                            {!! trans('Would you also like to be listed on the applicant marketplace so that you become visible to all partner companies and they can even contact you directly?') !!}
+                        </p>
+                    </div>
+                    <div class="flex items-start space-x-2">
+                        <label for="marketplacePrivacyPolicyAccepted" class="flex cursor-pointer items-center mb-0">
+                            <input
+                                id="marketplacePrivacyPolicyAccepted"
+                                wire:model="marketplacePrivacyPolicyAccepted"
+                                type="checkbox"
+                                class="flex-shrink-0 w-5 h-5 form-checkbox focus:border-primary-light focus:ring focus:ring-primary-light focus:ring-opacity-50 shadow-sm outline-none text-primary">
+                            <span class="checkbox-label flex-grow pl-2 text-sm">
+                                {!! trans('I have read and agree to the privacy policy.') !!}
+                            </span>
+                        </label>
+                    </div>
+                    <div class="flex flex-wrap items-end gap-4 btn-grp">
+                        <x-primary-button class="h-11" type="button" wire:click="showProfileMarketplace">
+                            {{ __('Yes') }}
+                        </x-primary-button>
+                        <x-secondary-button class="h-11" wire:click="DoNotShowProfileMarketplace" wire:loading.attr="disabled">
+                            {{ __('No') }}
+                        </x-secondary-button>
+                    </div>
                 </div>
-                <div class="flex flex-wrap items-end gap-4">
-                    <x-primary-button class="h-11" type="button" wire:click="showProfileMarketplace">
-                        {{ __('Yes') }}
-                    </x-primary-button>
-                    <x-secondary-button class="h-11" wire:click="DoNotShowProfileMarketplace" wire:loading.attr="disabled">
-                        {{ __('No') }}
-                    </x-secondary-button>
-                </div>
-                <div class="flex items-start space-x-2 mt-8">
-                    <label for="marketplacePrivacyPolicyAccepted" class="flex cursor-pointer items-center mb-0">
-                        <input
-                            id="marketplacePrivacyPolicyAccepted"
-                            wire:model="marketplacePrivacyPolicyAccepted"
-                            type="checkbox"
-                            class="flex-shrink-0 w-5 h-5 form-checkbox focus:border-primary-light focus:ring focus:ring-primary-light focus:ring-opacity-50 shadow-sm outline-none text-primary">
-                        <span class="checkbox-label flex-grow pl-2 text-sm">
-                            {!! trans('I have read and agree to the privacy policy.') !!}
-                        </span>
-                    </label>
-                </div>
+
             @endif
 
             @if((!is_null($user->show_application_on_marketplace_at) || !is_null($user->reject_marketplace_application_at)) && auth()->user()->application_status === ApplicationStatus::APPLIED_TO_SELECTED_COMPANY)
