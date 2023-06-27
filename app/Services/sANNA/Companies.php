3abc0e1bd49a25937a6416df5d6e5bfc06d5bceb
Pooja Jadav <contact2pooja36@gmail.com>
2023-06-27 17:10:52 +0530

    Refactor

--- app/Services/sANNA/Companies.php
+++ app/Services/sANNA/Companies.php
@@ -2,10 +2,6 @@
 
 namespace App\Services\sANNA;
 
-use App\Models\Company;
-use App\Models\CompanyContacts;
-use Exception;
-
 class Companies extends ErpService
 {
     protected $endpoint = '/platform/firmen';
